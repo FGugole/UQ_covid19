10673c669ca3c7dd33782bfb7c93b9798ce30622
Federica Gugole <federica@zonda.sc.cwi.nl>
2020-07-02 12:25:16 +0200

    included plot of distributions for FT

--- EasyVVUQ/plot_distributions.py
+++ EasyVVUQ/plot_distributions.py
@@ -12,20 +12,35 @@ import matplotlib.pyplot as plt
 plt.rcParams.update({'font.size': 20})
 plt.rcParams['figure.figsize'] = 8,6
 
-beta_1 = cp.Beta(alpha=3, beta=2, lower=.4)
+# Contact tracing
+beta_tpE = cp.Beta(alpha=3, beta=2, lower=.4)
+beta_tcr = cp.Beta(alpha=6, beta=2, lower=.4)
+gamma_trI = cp.Gamma(shape=2, scale=.4)
 
-beta_2 = cp.Beta(alpha=6, beta=2, lower=.4)
+x_CT = np.linspace(0, 1.5, 151)
 
-gamma_1 = cp.Gamma(shape=2, scale=.4)
-
-x = np.linspace(0, 1.5, 151)
-
-f = plt.figure('distributions')
+f = plt.figure('distributions_CT')
 ax = f.add_subplot(111, xlabel='x')
-ax.plot(x,beta_1.pdf(x),lw=2,label='trace_prob_E')
-ax.plot(x,gamma_1.pdf(x),lw=2,label='trace_rate_I')
-ax.plot(x,beta_2.pdf(x),lw=2,label='trace_cont_red')
+ax.plot(x_CT,beta_tpE.pdf(x_CT),lw=2,label='trace_prob_E')
+ax.plot(x_CT,gamma_trI.pdf(x_CT),lw=2,label='trace_rate_I')
+ax.plot(x_CT,beta_tcr.pdf(x_CT),lw=2,label='trace_cont_red')
 ax.set_xticks([0.0, .5, 1.0, 1.5])
+
+leg = plt.legend()
+leg.set_draggable(True)
+plt.tight_layout()
+
+# Flattening the curve
+beta_int1 = cp.Beta(alpha=3, beta=2, lower=.3, upper=.4)
+beta_up = cp.Beta(alpha=3, beta=2, lower=.5)
+
+x_FT = np.linspace(0, 1, 101)
+
+f = plt.figure('distributions_FT')
+ax = f.add_subplot(111, xlabel='x')
+ax.plot(x_FT,beta_int1.pdf(x_FT),lw=2,label='intervention_effect')
+ax.plot(x_FT,beta_up.pdf(x_FT),lw=2,label='uptake')
+ax.set_xticks([0.0, .5, 1.0])
 
 leg = plt.legend()
 leg.set_draggable(True)

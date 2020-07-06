a817d154e1529c5df5c06db3ae9b7b8adcc266e2
Federica Gugole <federica.gugole@cwi.nl>
2020-07-06 15:10:29 +0200

    post-processing file to generate empirical cdf of QoIs

---
+++ EasyVVUQ/cdf_post_proc_corona.py
@@ -0,0 +1,46 @@
+"""
+@author: Federica Gugole
+
+__license__= "LGPL"
+"""
+
+import numpy as np
+import easyvvuq as uq
+import os
+#import matplotlib as mpl
+#mpl.use('Agg')
+#from matplotlib import ticker
+import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 20})
+plt.rcParams['figure.figsize'] = 8,6
+
+"""
+*****************
+* VVUQ ANALYSES *
+*****************
+"""
+
+# home directory of this file    
+HOME = os.path.abspath(os.path.dirname(__file__))
+
+# Reload the campaign
+my_campaign = uq.Campaign(state_file = "campaign_state_FC_po3_moreuptake.json", work_dir = "/tmp")
+print('========================================================')
+print('Reloaded campaign', my_campaign.campaign_dir.split('/')[-1])
+print('========================================================')
+
+# get sampler and output columns from my_campaign object
+my_sampler = my_campaign._active_sampler
+output_columns = my_campaign._active_app_decoder.output_columns
+
+# collate output
+my_campaign.collate()
+# get full dataset of data
+data = my_campaign.get_collation_result()
+print(data)
+
+# Post-processing analysis
+sc_analysis = uq.analysis.SCAnalysis(sampler=my_sampler, qoi_cols=output_columns)
+my_campaign.apply_analysis(sc_analysis)
+
+results = my_campaign.get_last_analysis()

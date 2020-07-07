6089819880fea2990d755fe1c5185fd747075a7b
Federica Gugole <federica.gugole@cwi.nl>
2020-07-07 12:18:09 +0200

    fix bug

--- EasyVVUQ/plot_cdf.py
+++ EasyVVUQ/plot_cdf.py
@@ -12,10 +12,10 @@ p = np.arange(start=1,stop=n_runs+1,step=1)/n_runs
 
 f = plt.figure('cdfs',figsize=[12,6])
 ax_p = f.add_subplot(121, xlabel='IC_prev_avg_max', ylabel='cdf')
-ax_p.step(IC_prev_avg_max,p,lw=2)
+ax_p.step(QoI["IC_prev_avg_max"],p,lw=2)
 
 ax_e = f.add_subplot(122, xlabel='IC_ex_max', ylabel='cdf')
-ax_e.step(IC_ex_max,p,lw=2)
+ax_e.step(QoI["IC_ex_max"],p,lw=2)
 
 plt.tight_layout()
 f.savefig('figures/empirical_cdfs.png')

6001821af20d588f1cc2345e29aeba38e4d095b8
Federica Gugole <federica.gugole@cwi.nl>
2020-07-09 08:56:29 +0200

    start new bigger campaign for flattening the curve

--- EasyVVUQ/job_submission_corona.py
+++ EasyVVUQ/job_submission_corona.py
@@ -72,11 +72,11 @@ my_campaign.add_app(name="sc",
 # Create the sampler
 vary = {
     "seed": cp.DiscreteUniform(2**14, 2**16),
-    "trace_prob_E": cp.Beta(alpha=2, beta=4),
-    "trace_rate_I": cp.Gamma(shape=2, scale=.4),
-    "trace_contact_reduction": cp.Beta(alpha=10, beta=2),
-#    "intervention_effect": cp.Beta(alpha=38, beta=70),
-#    "uptake": cp.Beta(alpha=16, beta=2)
+#    "trace_prob_E": cp.Beta(alpha=2, beta=4),
+#    "trace_rate_I": cp.Gamma(shape=2, scale=.4),
+#    "trace_contact_reduction": cp.Beta(alpha=10, beta=2),
+    "intervention_effect": cp.Beta(alpha=38, beta=70),
+    "uptake": cp.Beta(alpha=16, beta=2)
 }
 
 #my_sampler = uq.sampling.SCSampler(vary=vary, polynomial_order=3, 
@@ -97,7 +97,7 @@ my_campaign.populate_runs_dir()
 
 # Run execution in parallel without Fabsim (using gnu parallel)
 cwd = os.getcwd()
-pcmd = f"ls -d {my_campaign.campaign_dir}/runs/Run_* | parallel -j 8 'cd {{}} ; Rscript {cwd}/contact_tracing_UQ.r corona_in.json > output.txt ; cd .. '"
+pcmd = f"ls -d {my_campaign.campaign_dir}/runs/Run_* | parallel -j 8 'cd {{}} ; Rscript {cwd}/flattening_the_curve_UQ.r corona_in.json > output.txt ; cd .. '"
 print('Parallel run command: ',pcmd)
 subprocess.call(pcmd,shell=True)
 

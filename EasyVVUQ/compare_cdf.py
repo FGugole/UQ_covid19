3fac975bba48a856f9fb254c1a98c760900c0b34
Federica Gugole <federica.gugole@cwi.nl>
2020-07-10 09:38:57 +0200

    script to plot and compare EasyVVUQ vs UQLab

---
+++ EasyVVUQ/compare_cdf.py
@@ -0,0 +1,126 @@
+"""
+@author: Federica Gugole
+
+__license__= "LGPL"
+"""
+
+import numpy as np
+import easyvvuq as uq
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 20})
+plt.rcParams['figure.figsize'] = 8,6
+
+"""
+*****************
+* VVUQ ANALYSES *
+*****************
+"""
+
+# home directory of this file    
+HOME = os.path.abspath(os.path.dirname(__file__))
+
+###################
+# Contact Tracing #
+###################
+# Reload the campaign
+campaign = uq.Campaign(state_file = "campaign_state_CT_MC100.json", work_dir = "/tmp")
+print('========================================================')
+print('Reloaded campaign', campaign.campaign_dir.split('/')[-1])
+print('========================================================')
+
+# collate output
+campaign.collate()
+# get full dataset of data
+data_CT = campaign.get_collation_result()
+
+n_runs = 100
+
+IC_prev_avg_max_CT = np.zeros(n_runs,dtype='float')
+IC_ex_max_CT = np.zeros(n_runs,dtype='float')
+
+for i in range(n_runs):
+    IC_prev_avg_max_CT[i] = data_CT.IC_prev_avg_max[i*L]
+    IC_ex_max_CT[i] = data_CT.IC_ex_max[i*L]
+
+IC_prev_avg_max_CT.sort()
+IC_ex_max_CT.sort()
+
+p = np.arange(start=1,stop=n_runs+1,step=1)/n_runs
+
+#################################
+# Load data from UQLab campaign #
+#################################
+QoI_UQLab_CT = pd.read_csv('../UQLab/runs_CT_MC100_updated_beta_gamma/CT_QoI.csv',delimiter=',',header=None)
+
+IC_prev_avg_max_UQLab_CT = np.copy(QoI_UQLab_CT.iloc[:,0])
+IC_ex_max_UQLab_CT = np.copy(QoI_UQLab_CT.iloc[:,1])
+tot_IC_UQLab_CT = np.copy(QoI_UQLab_CT.iloc[:,2])
+
+IC_prev_avg_max_UQLab_CT.sort()
+IC_ex_max_UQLab_CT.sort()
+
+f = plt.figure('cdfs',figsize=[12,6])
+ax_p = f.add_subplot(121, xlabel='maximum of patients in IC', ylabel='cdf')
+ax_p.step(IC_prev_avg_max_CT,p,lw=2,color='tab:blue')
+ax_p.step(IC_prev_avg_max_UQLab_CT,p,lw=2,color='tab:orange') 
+
+ax_e = f.add_subplot(122, xlabel='IC patient-days in excess', ylabel='cdf')
+ax_e.step(IC_ex_max_CT,p,lw=2,color='tab:blue',label='EasyVVUQ')
+ax_e.step(IC_ex_max_UQLab_CT,p,lw=2,color='tab:orange',label='UQLab')
+ax_e.legend(loc='best')
+
+plt.tight_layout()
+f.savefig('figures/cdf_CT_MC100_comparison.png')
+
+plt.show()
+
+########################
+# Flattening the curve #
+########################
+# Reload the campaign
+campaign = uq.Campaign(state_file = "campaign_state_FC_MC100.json", work_dir = "/tmp")
+print('========================================================')
+print('Reloaded campaign', campaign.campaign_dir.split('/')[-1])
+print('========================================================')
+
+# collate output
+campaign.collate()
+# get full dataset of data
+data_FC = campaign.get_collation_result()
+
+IC_prev_avg_max_FC = np.zeros(n_runs,dtype='float')
+IC_ex_max_FC = np.zeros(n_runs,dtype='float')
+
+for i in range(n_runs):
+    IC_prev_avg_max_FC[i] = data_FC.IC_prev_avg_max[i*L]
+    IC_ex_max_FC[i] = data_FC.IC_ex_max[i*L]
+
+IC_prev_avg_max_FC.sort()
+IC_ex_max_FC.sort()
+
+#################################
+# Load data from UQLab campaign #
+#################################
+QoI_UQLab_FC = pd.read_csv('../UQLab/runs_FC_MC100/FC_QoI.csv',delimiter=',',header=None)
+
+IC_prev_avg_max_UQLab_FC = np.copy(QoI_UQLab_FC.iloc[:,0])
+IC_ex_max_UQLab_FC = np.copy(QoI_UQLab_FC.iloc[:,1])
+tot_IC_UQLab_FC = np.copy(QoI_UQLab_FC.iloc[:,2])
+
+IC_prev_avg_max_UQLab_FC.sort()
+IC_ex_max_UQLab_FC.sort()
+
+f = plt.figure('cdfs',figsize=[12,6])
+ax_p = f.add_subplot(121, xlabel='maximum of patients in IC', ylabel='cdf')
+ax_p.step(IC_prev_avg_max_FC,p,lw=2,color='tab:blue')
+ax_p.step(IC_prev_avg_max_UQLab_FC,p,lw=2,color='tab:orange') 
+
+ax_e = f.add_subplot(122, xlabel='IC patient-days in excess', ylabel='cdf')
+ax_e.step(IC_ex_max_FC,p,lw=2,color='tab:blue',label='EasyVVUQ')
+ax_e.step(IC_ex_max_UQLab_FC,p,lw=2,color='tab:orange',label='UQLab')
+ax_e.legend(loc='best')
+
+plt.tight_layout()
+f.savefig('figures/cdf_FC_MC100_comparison.png')

1b840a213a7d8ec2db552f17a4121c6b4ad80f17
Federica Gugole <federica.gugole@cwi.nl>
2020-07-10 09:38:21 +0200

    moved the comparison with UQLab to another script

--- EasyVVUQ/cdf_post_proc_corona.py
+++ EasyVVUQ/cdf_post_proc_corona.py
@@ -28,8 +28,8 @@ print('Reloaded campaign', my_campaign.campaign_dir.split('/')[-1])
 print('========================================================')
 
 # get sampler and output columns from my_campaign object
-my_sampler = my_campaign._active_sampler
-output_columns = my_campaign._active_app_decoder.output_columns
+#my_sampler = my_campaign._active_sampler
+#output_columns = my_campaign._active_app_decoder.output_columns
 
 # collate output
 my_campaign.collate()
@@ -81,31 +81,6 @@ for i in range(n_runs-1):
     if (IC_ex_percentage[i]<IC_ex_threshold) & (IC_ex_percentage[i+1]>IC_ex_threshold):
         print('Probability that the percentage of IC patient days is below 5%:',p[i])
 
-# Reload the smaller campaign
-# campaign = uq.Campaign(state_file = "campaign_state_CT_MC100.json", work_dir = "/tmp")
-# print('========================================================')
-# print('Reloaded campaign', campaign.campaign_dir.split('/')[-1])
-# print('========================================================')
-
-# # collate output
-# campaign.collate()
-# # get full dataset of data
-# data_MC100 = campaign.get_collation_result()
-
-# n_r = 100
-
-# IC_prev_avg_max_MC100 = np.zeros(n_r,dtype='float')
-# IC_ex_max_MC100 = np.zeros(n_r,dtype='float')
-
-# for i in range(n_r):
-#     IC_prev_avg_max_MC100[i] = data_MC100.IC_prev_avg_max[i*L]
-#     IC_ex_max_MC100[i] = data_MC100.IC_ex_max[i*L]
-
-# IC_prev_avg_max_MC100.sort()
-# IC_ex_max_MC100.sort()
-
-# p_MC100 = np.arange(start=1,stop=n_r+1,step=1)/n_r
-
 f = plt.figure('cdfs',figsize=[12,6])
 ax_p = f.add_subplot(121, xlabel='maximum of patients in IC', ylabel='P(x)')
 ax_p.step(IC_prev_avg_max,p,lw=2,label='empirical cdf')
@@ -134,30 +109,5 @@ ax.axvline(x=IC_ex_threshold*100,color='tab:orange')
 
 plt.tight_layout()
 f.savefig('figures/cdf_FC_IC_ex_percentage')
-
-#################################
-# Load data from UQLab campaign #
-#################################
-# QoI_UQLab = pd.read_csv('../UQLab/runs_FC_MC100/FC_QoI.csv',delimiter=',',header=None)
-
-# IC_prev_avg_max_UQLab = np.copy(QoI_UQLab.iloc[:,0])
-# IC_ex_max_UQLab = np.copy(QoI_UQLab.iloc[:,1])
-# tot_IC_UQLab = np.copy(QoI_UQLab.iloc[:,2])
-
-# IC_prev_avg_max_UQLab.sort()
-# IC_ex_max_UQLab.sort()
-
-# f = plt.figure('cdfs',figsize=[12,6])
-# ax_p = f.add_subplot(121, xlabel='maximum of patients in IC', ylabel='cdf')
-# ax_p.step(IC_prev_avg_max,p,lw=2,color='tab:blue')
-# ax_p.step(IC_prev_avg_max_UQLab,p,lw=2,color='tab:orange') 
-
-# ax_e = f.add_subplot(122, xlabel='IC patient-days in excess', ylabel='cdf')
-# ax_e.step(IC_ex_max,p,lw=2,color='tab:blue',label='EasyVVUQ')
-# ax_e.step(IC_ex_max_UQLab,p,lw=2,color='tab:orange',label='UQLab')
-# ax_e.legend(loc='best')
-
-# plt.tight_layout()
-# f.savefig('figures/cdf_FC_MC100_comparison.png')
 
 plt.show()

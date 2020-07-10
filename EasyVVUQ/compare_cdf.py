6250f105834fb141c442934c7493fbd94692f5bb
Federica Gugole <federica.gugole@cwi.nl>
2020-07-10 09:47:56 +0200

    log scale for x-axis and renaming

--- EasyVVUQ/compare_cdf.py
+++ EasyVVUQ/compare_cdf.py
@@ -25,15 +25,15 @@ HOME = os.path.abspath(os.path.dirname(__file__))
 # Contact Tracing #
 ###################
 # Reload the campaign
-campaign = uq.Campaign(state_file = "campaign_state_CT_MC100.json", work_dir = "/tmp")
+CT_campaign = uq.Campaign(state_file = "campaign_state_CT_MC100.json", work_dir = "/tmp")
 print('========================================================')
-print('Reloaded campaign', campaign.campaign_dir.split('/')[-1])
+print('Reloaded campaign', CT_campaign.campaign_dir.split('/')[-1])
 print('========================================================')
 
 # collate output
-campaign.collate()
+CT_campaign.collate()
 # get full dataset of data
-data_CT = campaign.get_collation_result()
+data_CT = CT_campaign.get_collation_result()
 
 L = 551
 n_runs = 100
@@ -62,18 +62,20 @@ tot_IC_UQLab_CT = np.copy(QoI_UQLab_CT.iloc[:,2])
 IC_prev_avg_max_UQLab_CT.sort()
 IC_ex_max_UQLab_CT.sort()
 
-f = plt.figure('cdfs',figsize=[12,6])
-ax_p = f.add_subplot(121, xlabel='maximum of patients in IC', ylabel='cdf')
+f_CT = plt.figure('cdfs',figsize=[12,6])
+ax_p = f_CT.add_subplot(121, xlabel='maximum of patients in IC', ylabel='cdf')
 ax_p.step(IC_prev_avg_max_CT,p,lw=2,color='tab:blue')
 ax_p.step(IC_prev_avg_max_UQLab_CT,p,lw=2,color='tab:orange') 
+ax_p.set_xscale('log')
 
-ax_e = f.add_subplot(122, xlabel='IC patient-days in excess', ylabel='cdf')
+ax_e = f_CT.add_subplot(122, xlabel='IC patient-days in excess', ylabel='cdf')
 ax_e.step(IC_ex_max_CT,p,lw=2,color='tab:blue',label='EasyVVUQ')
 ax_e.step(IC_ex_max_UQLab_CT,p,lw=2,color='tab:orange',label='UQLab')
+ax_e.set_xscale('log')
 ax_e.legend(loc='best')
 
 plt.tight_layout()
-f.savefig('figures/cdf_CT_MC100_comparison.png')
+f_CT.savefig('figures/cdf_CT_MC100_comparison.png')
 
 plt.show()
 
@@ -81,15 +83,15 @@ plt.show()
 # Flattening the curve #
 ########################
 # Reload the campaign
-campaign = uq.Campaign(state_file = "campaign_state_FC_MC100.json", work_dir = "/tmp")
+FC_campaign = uq.Campaign(state_file = "campaign_state_FC_MC100.json", work_dir = "/tmp")
 print('========================================================')
-print('Reloaded campaign', campaign.campaign_dir.split('/')[-1])
+print('Reloaded campaign', FC_campaign.campaign_dir.split('/')[-1])
 print('========================================================')
 
 # collate output
-campaign.collate()
+FC_campaign.collate()
 # get full dataset of data
-data_FC = campaign.get_collation_result()
+data_FC = FC_campaign.get_collation_result()
 
 IC_prev_avg_max_FC = np.zeros(n_runs,dtype='float')
 IC_ex_max_FC = np.zeros(n_runs,dtype='float')
@@ -113,15 +115,17 @@ tot_IC_UQLab_FC = np.copy(QoI_UQLab_FC.iloc[:,2])
 IC_prev_avg_max_UQLab_FC.sort()
 IC_ex_max_UQLab_FC.sort()
 
-f = plt.figure('cdfs',figsize=[12,6])
-ax_p = f.add_subplot(121, xlabel='maximum of patients in IC', ylabel='cdf')
+f_FC = plt.figure('cdfs',figsize=[12,6])
+ax_p = f_FC.add_subplot(121, xlabel='maximum of patients in IC', ylabel='cdf')
 ax_p.step(IC_prev_avg_max_FC,p,lw=2,color='tab:blue')
 ax_p.step(IC_prev_avg_max_UQLab_FC,p,lw=2,color='tab:orange') 
+ax_p.set_xscale('log')
 
-ax_e = f.add_subplot(122, xlabel='IC patient-days in excess', ylabel='cdf')
+ax_e = f_FC.add_subplot(122, xlabel='IC patient-days in excess', ylabel='cdf')
 ax_e.step(IC_ex_max_FC,p,lw=2,color='tab:blue',label='EasyVVUQ')
 ax_e.step(IC_ex_max_UQLab_FC,p,lw=2,color='tab:orange',label='UQLab')
+ax_e.set_xscale('log')
 ax_e.legend(loc='best')
 
 plt.tight_layout()
-f.savefig('figures/cdf_FC_MC100_comparison.png')
+f_FC.savefig('figures/cdf_FC_MC100_comparison.png')

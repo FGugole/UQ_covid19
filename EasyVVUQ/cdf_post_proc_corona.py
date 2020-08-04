09f3d43fd60676441e3f479c462210ceef9ee19b
Federica Gugole <federica.gugole@cwi.nl>
2020-08-04 09:06:45 +0200

    fix ticks on x-axis

--- EasyVVUQ/cdf_post_proc_corona.py
+++ EasyVVUQ/cdf_post_proc_corona.py
@@ -9,6 +9,7 @@ import easyvvuq as uq
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib
 plt.rcParams.update({'font.size': 20})
 plt.rcParams['figure.figsize'] = 8,6
 
@@ -98,7 +99,8 @@ ax_p.step(IC_prev_avg_max,p-eps_DKW,linestyle='--',lw=2,color='tab:orange')
 #ax_p.axvline(x=IC_capacity,color='tab:orange')
 # ax_p.step(IC_prev_avg_max_MC100,p_MC100,lw=2,color='tab:olive',label='MC 100 runs')
 ax_p.set_xscale('log')
-#ax_p.set_xticks([1e2, 1e3])
+ax_p.set_xticks([3e2, 1e3])
+ax_p.get_xaxis().get_major_formatter().labelOnlyBase = False
 
 ax_e = f.add_subplot(122, xlabel='IC patient-days in excess', ylabel='P(x)')
 ax_e.step(IC_ex_max,p,lw=2)
@@ -106,7 +108,8 @@ ax_e.step(IC_ex_max,p+eps_DKW,linestyle='--',lw=2,color='tab:orange')
 ax_e.step(IC_ex_max,p-eps_DKW,linestyle='--',lw=2,color='tab:orange')
 #ax_e.step(IC_ex_max_MC100,p_MC100,lw=2,color='tab:olive')
 ax_e.set_xscale('log')
-#ax_e.set_xticks([1e4])
+ax_e.set_xticks([1e4, 6e4])
+ax_e.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
 
 ax_p.legend(loc='best')
 plt.tight_layout()

0bf8b9c8f7e24b2db4114a8df447127a1dce2e04
Federica Gugole <federica.gugole@cwi.nl>
2020-08-11 09:18:20 +0200

    file for computation of ensemble mean and variance

---
+++ EasyVVUQ/mean_var_post_proc_corona.py
@@ -0,0 +1,46 @@
+"""
+@author: Federica Gugole
+
+__license__= "LGPL"
+"""
+
+import numpy as np
+import easyvvuq as uq
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.ticker import ScalarFormatter, NullFormatter
+plt.rcParams.update({'font.size': 20})
+plt.rcParams['figure.figsize'] = 8,6
+
+"""
+*****************
+* VVUQ ANALYSES *
+*****************
+"""
+
+# home directory of this file    
+HOME = os.path.abspath(os.path.dirname(__file__))
+
+# Reload the campaign
+campaign = uq.Campaign(state_file = "campaign_state_PO_MC1000.json", work_dir = "/tmp")
+print('========================================================')
+print('Reloaded campaign', campaign.campaign_dir.split('/')[-1])
+print('========================================================')
+
+# get sampler and output columns from my_campaign object
+sampler = campaign._active_sampler
+#output_columns = my_campaign._active_app_decoder.output_columns
+
+# collate output
+campaign.collate()
+# get full dataset of data
+data = campaign.get_collation_result()
+#print(data.columns)
+
+# Post-processing analysis
+mc_analysis = uq.analysis.QMCAnalysis(qoi_cols=output_columns)
+campaign.apply_analysis(mc_analysis)
+
+results = campaign.get_last_analysis()
+print(results)

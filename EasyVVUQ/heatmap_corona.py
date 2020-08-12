cc272cf7fb4793ba9d56c87a105928e5ad326181
Federica Gugole <federica.gugole@cwi.nl>
2020-08-12 14:58:02 +0200

    set ticks

--- EasyVVUQ/heatmap_corona.py
+++ EasyVVUQ/heatmap_corona.py
@@ -68,9 +68,17 @@ ax_p = f.add_subplot(121, xlabel='intervention_effect', ylabel='uptake', title='
 im_p = ax_p.scatter(x=intervention_effect, y=uptake, c=IC_prev_avg_max, cmap='plasma')
 f.colorbar(im_p, ax=ax_p)
 
+ax_p.set_xticks([0.2, 0.35, 0.5])
+ax_p.set_yticks([0.6, 0.8, 1.0])
+
 ax_e = f.add_subplot(122, xlabel='intervention_effect', ylabel='uptake', title='IC_ex_max')
 im_e = ax_e.scatter(x=intervention_effect, y=uptake, c=IC_ex_max, cmap='plasma')
-f.colorbar(im_e, ax=ax_e)
+cbar_e = f.colorbar(im_e, ax=ax_e)
+cbar_e.set_ticks([0, 1e4, 2e4, 3e4])
+cbar_e.set_ticklabels([0, 1e4, 2e4, 3e4])
+
+ax_e.set_xticks([0.2, 0.35, 0.5])
+ax_e.set_yticks([0.6, 0.8, 1.0])
 
 plt.tight_layout()
 f.savefig('figures/heatmap_FC_MC1000.png')

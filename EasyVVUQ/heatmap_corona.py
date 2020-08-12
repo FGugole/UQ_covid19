9f9a6b4bc9c471c9363e512a7b32b29bc26c49f3
Federica Gugole <federica.gugole@cwi.nl>
2020-08-12 15:01:23 +0200

    fix labels on colorbar

--- EasyVVUQ/heatmap_corona.py
+++ EasyVVUQ/heatmap_corona.py
@@ -75,7 +75,7 @@ ax_e = f.add_subplot(122, xlabel='intervention_effect', ylabel='uptake', title='
 im_e = ax_e.scatter(x=intervention_effect, y=uptake, c=IC_ex_max, cmap='plasma')
 cbar_e = f.colorbar(im_e, ax=ax_e)
 cbar_e.set_ticks([0, 1e4, 2e4, 3e4])
-cbar_e.set_ticklabels([0, 1e4, 2e4, 3e4])
+cbar_e.set_ticklabels(['0', '10000', '20000', '30000'])
 
 ax_e.set_xticks([0.2, 0.35, 0.5])
 ax_e.set_yticks([0.6, 0.8, 1.0])

4a2ac0a719277d78aa4ad30bf014fbe9d3748268
Federica Gugole <federica.gugole@cwi.nl>
2020-08-13 09:53:46 +0200

    repeat simulation IL without bio

--- EasyVVUQ/job_submission_corona.py
+++ EasyVVUQ/job_submission_corona.py
@@ -117,9 +117,9 @@ vary = {
     # "trace_contact_reduction": cp.Beta(alpha=10, beta=2),
     # "intervention_effect": cp.Beta(alpha=38, beta=70),
     "lockdown_effect": cp.Beta(alpha=14, beta=42),
-    # "lockdown_length": cp.Gamma(shape=20, scale=2),
-    # "lift_length": cp.Gamma(shape=15, scale=1),
-    "phase_interval": cp.Gamma(shape=25, scale=2),
+    "lockdown_length": cp.Gamma(shape=20, scale=3),
+    "lift_length": cp.Gamma(shape=15, scale=2/3),
+    # "phase_interval": cp.Gamma(shape=25, scale=2),
     "uptake": cp.Beta(alpha=16, beta=2),
     # "Rzero": cp.Gamma(shape=100,scale=.025),
     # "duration_infectiousness": cp.Gamma(shape=25,scale=.2), 
@@ -144,7 +144,7 @@ my_campaign.populate_runs_dir()
 
 # Run execution in parallel without Fabsim (using gnu parallel)
 cwd = os.getcwd()
-pcmd = f"ls -d {my_campaign.campaign_dir}/runs/Run_* | parallel -j 8 'cd {{}} ; Rscript {cwd}/phased_opening_UQ.r corona_in.json > output.txt ; cd .. '"
+pcmd = f"ls -d {my_campaign.campaign_dir}/runs/Run_* | parallel -j 8 'cd {{}} ; Rscript {cwd}/intermittent_lockdown_UQ.r corona_in.json > output.txt ; cd .. '"
 print('Parallel run command: ',pcmd)
 subprocess.call(pcmd,shell=True)
 

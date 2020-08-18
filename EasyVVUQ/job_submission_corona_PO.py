c18096bd5b3402131e785125f33ea55a5ac8d090
Federica Gugole <federica.gugole@cwi.nl>
2020-08-18 08:32:22 +0200

    update figures for IL and start new campaign PO + bio

--- EasyVVUQ/job_submission_corona_PO.py
+++ EasyVVUQ/job_submission_corona_PO.py
@@ -115,10 +115,10 @@ vary = {
     "lockdown_effect": cp.Beta(alpha=14, beta=42),
     "phase_interval": cp.Gamma(shape=25, scale=2),
     "uptake": cp.Beta(alpha=16, beta=2),
-    # "Rzero": cp.Gamma(shape=100,scale=.025),
-    # "duration_infectiousness": cp.Gamma(shape=25,scale=.2), 
-    # "shape_exposed_time": cp.Gamma(shape=17.5,scale=1),
-    # "intervention_effect_var_inv": cp.Gamma(shape=2,scale=.05)
+    "Rzero": cp.Gamma(shape=100,scale=.025),
+    "duration_infectiousness": cp.Gamma(shape=25,scale=.2), 
+    "shape_exposed_time": cp.Gamma(shape=17.5,scale=1),
+    "intervention_effect_var_inv": cp.Gamma(shape=2,scale=.05)
 }
 
 #my_sampler = uq.sampling.SCSampler(vary=vary, polynomial_order=3, 
@@ -138,7 +138,7 @@ my_campaign.populate_runs_dir()
 
 # Run execution in parallel without Fabsim (using gnu parallel)
 cwd = os.getcwd()
-pcmd = f"ls -d {my_campaign.campaign_dir}/runs/Run_* | parallel -j 8 'cd {{}} ; Rscript {cwd}/phased_opening_UQ.r corona_in.json > output.txt ; cd .. '"
+pcmd = f"ls -d {my_campaign.campaign_dir}/runs/Run_* | parallel -j 8 'cd {{}} ; Rscript {cwd}/phased_opening_UQ_bio.r corona_in.json > output.txt ; cd .. '"
 print('Parallel run command: ',pcmd)
 subprocess.call(pcmd,shell=True)
 

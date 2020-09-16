de5d1fdd7464feecffb35c199e85424d975d488b
Federica Gugole <federica.gugole@cwi.nl>
2020-09-16 09:37:08 +0200

    attempting to have asymmetric errorbars

--- EasyVVUQ/sobol_corona.py
+++ EasyVVUQ/sobol_corona.py
@@ -59,6 +59,34 @@ params = list(my_sampler.vary.get_keys())
 time = np.arange(0, 550+1, 1)
 
 ######################################################################
+#colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple']
+sobol_idx_ICp = np.zeros((1,len(params)), dtype='float')
+yerr_ICp = np.zeros((2,len(params)), dtype='float')
+
+sobol_idx_ICe = np.zeros((1,len(params)), dtype='float')
+yerr_ICe = np.zeros((2,len(params)), dtype='float')
+
+idx = 0
+for param in params: 
+    #
+    sobol_idx = sobols['IC_prev_avg_max'][param][200]
+    sobol_idx_ICp[idx] = sobol_idx
+    low = results['conf_sobols_first']['IC_prev_avg_max'][param]['low'][200]
+    high = results['conf_sobols_first']['IC_prev_avg_max'][param]['high'][200]
+    yerr_ICp[:,idx] = [sobol_idx-low, high-sobol_idx]
+    #
+    sobol_idx = sobols['IC_ex_max'][param][200]
+    sobol_idx_ICe[idx] = sobol_idx
+    low = results['conf_sobols_first']['IC_ex_max'][param]['low'][200]
+    high = results['conf_sobols_first']['IC_ex_max'][param]['high'][200]
+    yerr_ICe[:,idx] = [sobol_idx-low, high-sobol_idx]
+    #
+    idx += 1
+    # print values to terminal
+    print('Param = ',param)
+    print('Sobol index for IC_prev_avg_max = ', sobols['IC_prev_avg_max'][param][200])
+    print('Sobol index for IC_ex_max = ', sobols['IC_ex_max'][param][200])
+
 f = plt.figure('Sobol_IC_max', figsize=[12, 6])
 ax_ICp_max = f.add_subplot(121, title = 'IC_prev_avg_max')
 ax_ICp_max.set_ylim([-.1, 1.1])
@@ -66,26 +94,8 @@ ax_ICp_max.set_ylim([-.1, 1.1])
 ax_ICe_max = f.add_subplot(122, title = 'IC_ex_max')
 ax_ICe_max.set_ylim([-.1, 1.1])
 
-colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple']
-idx = 0
-for param in params: 
-    sobol_idx = sobols['IC_prev_avg_max'][param][200]
-    low = results['conf_sobols_first']['IC_prev_avg_max'][param]['low'][200]
-    high = results['conf_sobols_first']['IC_prev_avg_max'][param]['high'][200]
-    yerr = (high-low)/2 #[sobol_idx-low, high-sobol_idx]
-    ax_ICp_max.errorbar(idx, sobol_idx, yerr=yerr, fmt='o')#, ecolor=colors[idx])
-    #
-    sobol_idx = sobols['IC_ex_max'][param][200]
-    low = results['conf_sobols_first']['IC_ex_max'][param]['low'][200]
-    high = results['conf_sobols_first']['IC_ex_max'][param]['high'][200]
-    yerr = (high-low)/2 #[sobol_idx-low, high-sobol_idx]
-    ax_ICe_max.errorbar(idx, sobol_idx, yerr=yerr, fmt='o')#, ecolor=colors[idx])
-    #
-    idx += 1
-    # print values to terminal
-    print('Param = ',param)
-    print('Sobol index for IC_prev_avg_max = ', sobols['IC_prev_avg_max'][param][200])
-    print('Sobol index for IC_ex_max = ', sobols['IC_ex_max'][param][200])
+ax_ICp_max.errorbar(np.arange(0, len(params), 1), sobol_idx_ICp, yerr=yerr_ICp, fmt='o')#, ecolor=colors[idx])
+ax_ICe_max.errorbar(np.arange(0, len(params), 1), sobol_idx_ICe, yerr=yerr_ICe, fmt='o')#, ecolor=colors[idx])
 
 ax_ICp_max.set_xticks(np.arange(0, len(params), 1))
 ax_ICp_max.set_xticklabels(params, rotation=45)

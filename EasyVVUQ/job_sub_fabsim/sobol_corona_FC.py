0d679b4a8be955b893b498ffbc2038cebad157c9
Federica Gugole <federica.gugole@cwi.nl>
2020-11-23 11:46:52 +0100

    updated scripts as on desktop computer in the office

--- EasyVVUQ/job_sub_fabsim/sobol_corona_FC.py
+++ EasyVVUQ/job_sub_fabsim/sobol_corona_FC.py
@@ -5,8 +5,11 @@ __license__= "LGPL"
 """
 
 import numpy as np
+#import pandas as pd
 import easyvvuq as uq
 import os
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 plt.rcParams.update({'font.size': 20})
 plt.rcParams['figure.figsize'] = 8,6
@@ -20,13 +23,13 @@ import fabsim3_cmd_api as fab
 config = 'virsim'
 script = 'virsim_FC'
 machine = 'eagle_vecma'
-workdir = '/home/federica/Desktop/VirsimCampaigns'#'/tmp'
+workdir = '/ufs/federica/Desktop/VirsimCampaigns'#'/tmp'
 
 # home directory of this file    
 HOME = os.path.abspath(os.path.dirname(__file__))
 
 # Reload the campaign
-campaign = uq.Campaign(state_file = "campaign_state_FC_MC5000.json", work_dir = workdir)
+campaign = uq.Campaign(state_file = "campaign_state_FC.json", work_dir = workdir)
 print('========================================================')
 print('Reloaded campaign', campaign.campaign_dir.split('/')[-1])
 print('========================================================')
@@ -35,28 +38,34 @@ print('========================================================')
 sampler = campaign._active_sampler
 # print(type(sampler._samples))
 # print(sampler._samples.shape)
-sampler._n_samples = 5000
 
-output_columns = campaign._active_app_decoder.output_columns
+# output_columns = campaign._active_app_decoder.output_columns
+
+#Manually specify a subset of the output QoIs, is faster
+output_columns = ["IC_prev_avg_max", "IC_ex_max"]
 
 fab.verify(config, campaign.campaign_dir, 
             campaign._active_app_decoder.target_filename, 
-            machine=machine, PilotJob=True)
+            machine=machine, PJ=True)
 
 fab.get_uq_samples(config, campaign.campaign_dir, sampler._n_samples,
-                   skip=0, machine='eagle_vecma')
+                    skip=0, machine='eagle_vecma')
 
 # collate output
 campaign.collate()
 # get full dataset of data
 data = campaign.get_collation_result()
-#print(data)
+# print(data)
 
 # Post-processing analysis
 qmc_analysis = uq.analysis.QMCAnalysis(sampler=sampler, qoi_cols=output_columns)
-campaign.apply_analysis(qmc_analysis)
+# campaign.apply_analysis(qmc_analysis)
 
-results = campaign.get_last_analysis()
+#manually execute analyse, such that we can supply output_index=-1, only using the last entry
+#of the 551 points as QoI
+results = qmc_analysis.analyse(data, output_index=-1)
+
+# results = campaign.get_last_analysis()
 #print(results)
 
 """
@@ -69,7 +78,7 @@ sobols = results['sobols_first']
 params = list(sampler.vary.get_keys())
 #print(params)
 
-time = np.arange(0, 550+1, 1)
+#time = np.arange(0, 550+1, 1)
 
 ######################################################################
 sobol_idx_ICp = np.zeros((len(params)), dtype='float')
@@ -81,35 +90,40 @@ yerr_ICe = np.zeros((2,len(params)), dtype='float')
 idx = 0
 for param in params: 
     #
-    sobol_idx = sobols['IC_prev_avg_max'][param][200]
+    sobol_idx = sobols['IC_prev_avg_max'][param]
     sobol_idx_ICp[idx] = sobol_idx
-    low = results['conf_sobols_first']['IC_prev_avg_max'][param]['low'][200]
-    high = results['conf_sobols_first']['IC_prev_avg_max'][param]['high'][200]
+    low = results['conf_sobols_first']['IC_prev_avg_max'][param]['low']
+    high = results['conf_sobols_first']['IC_prev_avg_max'][param]['high']
     yerr_ICp[:,idx] = [sobol_idx-low, high-sobol_idx]
     #
-    sobol_idx = sobols['IC_ex_max'][param][200]
+    sobol_idx = sobols['IC_ex_max'][param]
     sobol_idx_ICe[idx] = sobol_idx
-    low = results['conf_sobols_first']['IC_ex_max'][param]['low'][200]
-    high = results['conf_sobols_first']['IC_ex_max'][param]['high'][200]
+    low = results['conf_sobols_first']['IC_ex_max'][param]['low']
+    high = results['conf_sobols_first']['IC_ex_max'][param]['high']
     yerr_ICe[:,idx] = [sobol_idx-low, high-sobol_idx]
     #
     idx += 1
     # print values to terminal
     print('Param = ',param)
-    print('Sobol index for IC_prev_avg_max = ', sobols['IC_prev_avg_max'][param][200])
-    print('Sobol index for IC_ex_max = ', sobols['IC_ex_max'][param][200])
+    print('Sobol index for IC_prev_avg_max = ', sobols['IC_prev_avg_max'][param])
+    print('95% CI lower bound = ', results['conf_sobols_first']['IC_prev_avg_max'][param]['low'])
+    print('95% CI upper bound = ', results['conf_sobols_first']['IC_prev_avg_max'][param]['high'])
 
-f = plt.figure('Sobol_IC_max', figsize=[12, 6])
-ax_ICp_max = f.add_subplot(121, title = 'IC_prev_avg_max')
+    print('Sobol index for IC_ex_max = ', sobols['IC_ex_max'][param])
+    print('95% CI lower bound = ', results['conf_sobols_first']['IC_ex_max'][param]['low'])
+    print('95% CI upper bound = ', results['conf_sobols_first']['IC_ex_max'][param]['high'])
+
+f = plt.figure('Sobol_IC_max', figsize=[12,7])
+ax_ICp_max = f.add_subplot(121, title = 'Maximum of patients in IC')
 ax_ICp_max.set_ylim([-.1, 1.1])
 
-ax_ICe_max = f.add_subplot(122, title = 'IC_ex_max')
+ax_ICe_max = f.add_subplot(122, title = 'IC patient-days in excess')
 ax_ICe_max.set_ylim([-.1, 1.1])
 
 ax_ICp_max.errorbar(np.arange(0, len(params), 1), sobol_idx_ICp, yerr=yerr_ICp, \
-    fmt='o', elinewidth=2, color='teal')
+    fmt='o', elinewidth=2, color='forestgreen')
 ax_ICe_max.errorbar(np.arange(0, len(params), 1), sobol_idx_ICe, yerr=yerr_ICe, \
-    fmt='o', elinewidth=2, color='teal')
+    fmt='o', elinewidth=2, color='forestgreen')
 
 ax_ICp_max.set_xticks(np.arange(0, len(params), 1))
 ax_ICp_max.set_xticklabels(params, rotation=45)
@@ -121,4 +135,4 @@ f.savefig('figures/Sobol_IC_max_FC.png')
 
 plt.show()
 
-### END OF CODE ###
+# ### END OF CODE ###
--- EasyVVUQ/job_sub_fabsim/sobol_corona_PO.py
+++ EasyVVUQ/job_sub_fabsim/sobol_corona_PO.py
@@ -7,9 +7,11 @@ __license__= "LGPL"
 import numpy as np
 import easyvvuq as uq
 import os
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 plt.rcParams.update({'font.size': 20})
-plt.rcParams['figure.figsize'] = 8,6
+plt.rcParams['figure.figsize'] = 8,5
 import fabsim3_cmd_api as fab
 """
 *****************
@@ -20,7 +22,7 @@ import fabsim3_cmd_api as fab
 config = 'virsim_PO'
 script = 'virsim_PO'
 machine = 'eagle_vecma'
-workdir = '/home/federica/Desktop/VirsimCampaigns'#'/tmp'
+workdir = '/ufs/federica/Desktop/VirsimCampaigns'#'/tmp'
 
 # home directory of this file    
 HOME = os.path.abspath(os.path.dirname(__file__))
@@ -40,10 +42,12 @@ output_columns = campaign._active_app_decoder.output_columns
 
 fab.verify(config, campaign.campaign_dir, 
             campaign._active_app_decoder.target_filename, 
-            machine=machine, PilotJob=True)
+            machine=machine, PJ=True)
 
-fab.get_uq_samples(config, campaign.campaign_dir, sampler._n_samples,
-                   skip=0, machine='eagle_vecma')
+# fab.fetch_results(machine=machine)
+
+fab.get_uq_samples(config, campaign.campaign_dir, sampler.n_samples(),
+                   skip=0, machine=machine)
 
 # collate output
 campaign.collate()
@@ -53,9 +57,9 @@ data = campaign.get_collation_result()
 
 # Post-processing analysis
 qmc_analysis = uq.analysis.QMCAnalysis(sampler=sampler, qoi_cols=output_columns)
-campaign.apply_analysis(qmc_analysis)
-
-results = campaign.get_last_analysis()
+#campaign.apply_analysis(qmc_analysis)
+#results = campaign.get_last_analysis()
+results = qmc_analysis.analyse(data, output_index=-1)
 #print(results)
 
 """
@@ -68,7 +72,7 @@ sobols = results['sobols_first']
 params = list(sampler.vary.get_keys())
 #print(params)
 
-time = np.arange(0, 550+1, 1)
+# time = np.arange(0, 550+1, 1)
 
 ######################################################################
 sobol_idx_ICp = np.zeros((len(params)), dtype='float')
@@ -80,25 +84,30 @@ yerr_ICe = np.zeros((2,len(params)), dtype='float')
 idx = 0
 for param in params: 
     #
-    sobol_idx = sobols['IC_prev_avg_max'][param][200]
+    sobol_idx = sobols['IC_prev_avg_max'][param]#[200]
     sobol_idx_ICp[idx] = sobol_idx
-    low = results['conf_sobols_first']['IC_prev_avg_max'][param]['low'][200]
-    high = results['conf_sobols_first']['IC_prev_avg_max'][param]['high'][200]
+    low = results['conf_sobols_first']['IC_prev_avg_max'][param]['low']#[200]
+    high = results['conf_sobols_first']['IC_prev_avg_max'][param]['high']#[200]
     yerr_ICp[:,idx] = [sobol_idx-low, high-sobol_idx]
     #
-    sobol_idx = sobols['IC_ex_max'][param][200]
+    sobol_idx = sobols['IC_ex_max'][param]#[200]
     sobol_idx_ICe[idx] = sobol_idx
-    low = results['conf_sobols_first']['IC_ex_max'][param]['low'][200]
-    high = results['conf_sobols_first']['IC_ex_max'][param]['high'][200]
+    low = results['conf_sobols_first']['IC_ex_max'][param]['low']#[200]
+    high = results['conf_sobols_first']['IC_ex_max'][param]['high']#[200]
     yerr_ICe[:,idx] = [sobol_idx-low, high-sobol_idx]
     #
     idx += 1
     # print values to terminal
     print('Param = ',param)
-    print('Sobol index for IC_prev_avg_max = ', sobols['IC_prev_avg_max'][param][200])
-    print('Sobol index for IC_ex_max = ', sobols['IC_ex_max'][param][200])
+    print('Sobol index for IC_prev_avg_max = ', sobols['IC_prev_avg_max'][param])#[200])
+    print('95% CI lower bound = ', results['conf_sobols_first']['IC_prev_avg_max'][param]['low'])#[200])
+    print('95% CI upper bound = ', results['conf_sobols_first']['IC_prev_avg_max'][param]['high'])#[200])
 
-f = plt.figure('Sobol_IC_max', figsize=[12, 6])
+    print('Sobol index for IC_ex_max = ', sobols['IC_ex_max'][param])#[200])
+    print('95% CI lower bound = ', results['conf_sobols_first']['IC_ex_max'][param]['low'])#[200])
+    print('95% CI upper bound = ', results['conf_sobols_first']['IC_ex_max'][param]['high'])#[200])
+
+f = plt.figure('Sobol_IC_max', figsize=[12,7])
 ax_ICp_max = f.add_subplot(121, title = 'IC_prev_avg_max')
 ax_ICp_max.set_ylim([-.1, 1.1])
 
@@ -106,9 +115,9 @@ ax_ICe_max = f.add_subplot(122, title = 'IC_ex_max')
 ax_ICe_max.set_ylim([-.1, 1.1])
 
 ax_ICp_max.errorbar(np.arange(0, len(params), 1), sobol_idx_ICp, yerr=yerr_ICp, \
-    fmt='o', elinewidth=2, color='teal')
+    fmt='o', elinewidth=2, color='forestgreen')
 ax_ICe_max.errorbar(np.arange(0, len(params), 1), sobol_idx_ICe, yerr=yerr_ICe, \
-    fmt='o', elinewidth=2, color='teal')
+    fmt='o', elinewidth=2, color='forestgreen')
 
 ax_ICp_max.set_xticks(np.arange(0, len(params), 1))
 ax_ICp_max.set_xticklabels(params, rotation=45)

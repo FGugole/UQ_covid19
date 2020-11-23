da814def5e4b39677035e9becc478d3636769099
Federica Gugole <federica.gugole@cwi.nl>
2020-11-23 11:49:32 +0100

    write full output dataset

--- EasyVVUQ/job_sub_fabsim/flattening_the_curve_UQ.r
+++ EasyVVUQ/job_sub_fabsim/flattening_the_curve_UQ.r
@@ -131,7 +131,7 @@ QoI <- data.frame("IC_prev_avg_max"=max(na.omit(IC_prev_avg)), "IC_ex_max"=max(I
 #############################
 # Write results to csv file #
 #############################
-#write.csv(x=flat_curve_data, file=output_filename, row.names=FALSE)
-write.csv(x=QoI, file=output_filename, row.names=FALSE)
+write.csv(x=flat_curve_data, file=output_filename, row.names=FALSE)
+#write.csv(x=QoI, file=output_filename, row.names=FALSE)
 
 ### END OF CODE ###

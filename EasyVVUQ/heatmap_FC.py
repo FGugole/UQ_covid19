44b63a5e958d40ace4e42dd0e736eeab944a114b
Federica Gugole <federica.gugole@cwi.nl>
2020-12-14 15:40:30 +0100

    modified such that it works with the latest release of EasyVVUQ

--- EasyVVUQ/heatmap_FC.py
+++ EasyVVUQ/heatmap_FC.py
@@ -23,17 +23,18 @@ plt.rcParams['figure.figsize'] = 8,6
 HOME = os.path.abspath(os.path.dirname(__file__))
 
 # Reload the campaign
-my_campaign = uq.Campaign(state_file = "campaign_state_FC_MC1000.json", work_dir = "/tmp")
+workdir = '/export/scratch2/home/federica/'
+my_campaign = uq.Campaign(state_file = "campaign_state_FC_bio_1e2.json", work_dir = workdir)
 print('========================================================')
 print('Reloaded campaign', my_campaign.campaign_dir.split('/')[-1])
 print('========================================================')
 
-# get sampler and output columns from my_campaign object
+# get sampler from my_campaign object
 my_sampler = my_campaign._active_sampler
-#output_columns = my_campaign._active_app_decoder.output_columns
 
 # collate output
 my_campaign.collate()
+
 # get full dataset of data
 data = my_campaign.get_collation_result()
 #print(data.columns)
@@ -43,27 +44,19 @@ IC_capacity = 109
 
 n_runs = 1000
 
-intervention_effect = np.zeros(n_runs,dtype='float')
-uptake = np.zeros(n_runs,dtype='float')
-IC_prev_avg_max = np.zeros(n_runs,dtype='float')
-IC_ex_max = np.zeros(n_runs,dtype='float')
+intervention_effect = data['intervention_effect',0] 
+intervention_effect = intervention_effect.to_numpy()
 
-markers = []
+uptake = data['uptake',0] 
+uptake = uptake.to_numpy()
 
-for i in range(n_runs):
-    IC_prev_avg_max[i] = data.IC_prev_avg_max[i*L]
-    IC_ex_max[i] = data.IC_ex_max[i*L]
+IC_prev_avg_max = data['IC_prev_avg_max',0] 
+IC_prev_avg_max = IC_prev_avg_max.to_numpy()
 
-params = list(my_sampler.vary.get_keys())
-# Save parameters values used in the simulations
-cnt = 0
-info = my_campaign.list_runs()
-for run in info:
-	intervention_effect[cnt] = run[1]['params']['intervention_effect']
-	uptake[cnt] = run[1]['params']['uptake']
-	cnt += 1
-#     print(run[0])
-#     print(run[1]['params'])
+IC_ex_max = data['IC_ex_max',0] 
+IC_ex_max = IC_ex_max.to_numpy()
+
+# Plot
 
 f = plt.figure('heatmap',figsize=[12,6])
 ax_p = f.add_subplot(121, xlabel='Effect of intervention', ylabel='Uptake by the population')
@@ -75,7 +68,7 @@ cbar_p = f.colorbar(im_p, ax=ax_p)
 cbar_p.set_ticks([150, 250, 350, 450])
 cbar_p.set_ticklabels(['150', '250', '350', '450'])
 
-ax_p.set_xticks([0.2, 0.35, 0.5])
+ax_p.set_xticks([0.25, 0.35, 0.45])
 ax_p.set_yticks([0.6, 0.8, 1.0])
 
 ax_e = f.add_subplot(122, xlabel='Effect of intervention')
@@ -87,11 +80,11 @@ cbar_e = f.colorbar(im_e, ax=ax_e)
 cbar_e.set_ticks([1e4, 2e4, 3e4])
 cbar_e.set_ticklabels(['10000', '20000', '30000'])
 
-ax_e.set_xticks([0.2, 0.35, 0.5])
+ax_e.set_xticks([0.25, 0.35, 0.45])
 ax_e.set_yticks([0.6, 0.8, 1.0])
 
 plt.tight_layout()
-f.savefig('figures/heatmap_FC_MC1000.png')
+f.savefig('figures/heatmap_FC_MC100.png')
 
 plt.show()
 

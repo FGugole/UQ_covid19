09ecd7737a46efde60b9ef13012e05dba0a8556d
Federica Gugole <federica.gugole@cwi.nl>
2020-12-15 11:46:02 +0100

    adapted code to run with latest release of EasyVVUQ

--- EasyVVUQ/cdf_post_proc_corona.py
+++ EasyVVUQ/cdf_post_proc_corona.py
@@ -23,7 +23,8 @@ plt.rcParams['figure.figsize'] = 8,6
 HOME = os.path.abspath(os.path.dirname(__file__))
 
 # Reload the campaign without biology
-campaign = uq.Campaign(state_file = "campaign_state_CT_MC1000.json", work_dir = "/tmp")
+workdir = '/export/scratch2/home/federica/'
+campaign = uq.Campaign(state_file = "campaign_state_CT_nobio_1e2.json", work_dir = workdir)
 print('========================================================')
 print('Reloaded campaign', campaign.campaign_dir.split('/')[-1])
 print('========================================================')
@@ -39,14 +40,13 @@ data = campaign.get_collation_result()
 #print(data.columns)
 
 # Reload the campaign with biology
-campaign_bio = uq.Campaign(state_file = "campaign_state_CT_bio_MC1000.json", work_dir = "/tmp")
+campaign_bio = uq.Campaign(state_file = "campaign_state_CT_bio_1e2.json", work_dir = workdir)
 print('========================================================')
 print('Reloaded campaign', campaign_bio.campaign_dir.split('/')[-1])
 print('========================================================')
 
-# get sampler and output columns from my_campaign object
+# get sampler from my_campaign object
 sampler_bio = campaign_bio._active_sampler
-#output_columns = my_campaign._active_app_decoder.output_columns
 
 # collate output
 campaign_bio.collate()
@@ -59,32 +59,33 @@ data_bio = campaign_bio.get_collation_result()
 * Empirical CDF of QoIs *
 *************************
 """
-L = 551 
+
 IC_capacity = 109
-IC_ex_threshold = 0.05
 
-n_runs = 1000
+IC_prev_avg_max = data['IC_prev_avg_max',0] 
+IC_prev_avg_max = IC_prev_avg_max.to_numpy()
 
-IC_prev_avg_max = np.zeros(n_runs,dtype='float')
-IC_ex_max = np.zeros(n_runs,dtype='float')
-IC_prev_avg_max_bio = np.zeros(n_runs,dtype='float')
-IC_ex_max_bio = np.zeros(n_runs,dtype='float')
+IC_ex_max = data['IC_ex_max',0] 
+IC_ex_max = IC_ex_max.to_numpy()
 
-for i in range(n_runs):
-	# without biology
-    IC_prev_avg_max[i] = data.IC_prev_avg_max[i*L]
-    IC_ex_max[i] = data.IC_ex_max[i*L]
-    # with biology
-    IC_prev_avg_max_bio[i] = data_bio.IC_prev_avg_max[i*L]
-    IC_ex_max_bio[i] = data_bio.IC_ex_max[i*L]
+IC_prev_avg_max_bio = data_bio['IC_prev_avg_max',0] 
+IC_prev_avg_max_bio = IC_prev_avg_max_bio.to_numpy()
 
+IC_ex_max_bio = data_bio['IC_ex_max',0] 
+IC_ex_max_bio = IC_ex_max_bio.to_numpy()
+
+# Sort values of the QoIs in incresing order
 IC_prev_avg_max.sort()
 IC_ex_max.sort()
+
 IC_prev_avg_max_bio.sort()
 IC_ex_max_bio.sort()
 
-print('Minimum value in the peak of IC patients without biology = ',min(IC_prev_avg_max))
-print('Minimum value in the peak of IC patients with biology = ',min(IC_prev_avg_max_bio))
+print('Minimum value in the peak of IC patients without biology = ', min(IC_prev_avg_max))
+print('Minimum value in the peak of IC patients with biology = ', min(IC_prev_avg_max_bio))
+
+n_runs = len(IC_prev_avg_max)
+#print('n_runs = ', n_runs)
 
 p = np.arange(start=1,stop=n_runs+1,step=1)/n_runs
 
@@ -92,20 +93,18 @@ alpha_DKW = 0.05
 eps_DKW = np.sqrt( np.log(2/alpha_DKW) / (2*n_runs) )
 
 for i in range(n_runs-1):
-	# wihout biology
+    # wihout biology
     if (IC_prev_avg_max[i]<IC_capacity) & (IC_prev_avg_max[i+1]>IC_capacity):
         print('Probability that the maximum number of IC patient is below IC capacity (without biology):',p[i])
     # with biology
     if (IC_prev_avg_max_bio[i]<IC_capacity) & (IC_prev_avg_max_bio[i+1]>IC_capacity):
         print('Probability that the maximum number of IC patient is below IC capacity (with biology):',p[i])
 
-params = list(sampler.vary.get_keys())
-bio_params = list(sampler_bio.vary.get_keys())
-# Print parameters values used in the simulations
-info = campaign.list_runs()
-# for run in info:
-#     print(run[0])
-#     print(run[1]['params'])
+'''
+********
+* Plot *
+********
+'''
 
 f = plt.figure('cdfs',figsize=[12,6])
 ax_p = f.add_subplot(121, xlabel='maximum of patients in IC', ylabel='P(x)')
@@ -135,13 +134,12 @@ ax_e.step(IC_ex_max_bio,p-eps_DKW,linestyle='--',lw=2,color='indianred')
 # general settings
 ax_e.set_xscale('log')
 # ax_e.set_xticks([1e4, 6e4])
-#ax_e.get_xaxis().set_major_formatter(ScalarFormatter())
 ax_e.get_xaxis().get_major_formatter().labelOnlyBase = False
 ax_e.get_xaxis().set_minor_formatter(NullFormatter())
 
 ax_p.legend(loc='best')
 plt.tight_layout()
-f.savefig('figures/cdf_CT_MC1000.png')
+f.savefig('figures/cdf_CT.png')
 
 plt.show()
 

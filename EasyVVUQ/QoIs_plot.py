abd0f7e3b90f215ee1979e5ea61b36567c3a614f
Federica Gugole <federica.gugole@cwi.nl>
2021-02-12 11:56:23 +0100

    adapt script to v0.8 of EasyVVUQ

--- EasyVVUQ/QoIs_plot.py
+++ EasyVVUQ/QoIs_plot.py
@@ -19,7 +19,7 @@ plt.rcParams['figure.figsize'] = 12,9
 * Load data *
 *************
 """
-workdir = '/home/federica/Desktop/VirsimCampaigns'#'/tmp'
+workdir = '/export/scratch2/home/federica/'
 
 # home directory of this file    
 HOME = os.path.abspath(os.path.dirname(__file__))
@@ -48,21 +48,18 @@ time = np.arange(start=0, stop=L, step=1)
 IC_capacity = 109
 
 n_runs2plot = 4
-#runs2plot = cp.DiscreteUniform(0, 999).sample(n_runs2plot)
+#runs2plot = cp.DiscreteUniform(0, 49).sample(n_runs2plot)
 #print(runs2plot)
-runs2plot = [178, 757, 945, 827]
+runs2plot = [18, 7, 45, 27]
 
 IC_prev_PO = np.zeros((L,n_runs2plot), dtype='float')
 IC_prev_avg_PO = np.zeros((L,n_runs2plot), dtype='float')
 IC_ex_PO = np.zeros((L,n_runs2plot), dtype='float')
 
 for i in range(n_runs2plot):
-	IC_prev_PO[:,i] = PO_data.IC_prev[(PO_data.index >= runs2plot[i]*L) & 
-										(PO_data.index < (1+runs2plot[i])*L)]
-	IC_prev_avg_PO[:,i] = PO_data.IC_prev_avg[(PO_data.index >= runs2plot[i]*L) & 
-												(PO_data.index < (1+runs2plot[i])*L)]
-	IC_ex_PO[:,i] = PO_data.IC_ex[(PO_data.index >= runs2plot[i]*L) & 
-										(PO_data.index < (1+runs2plot[i])*L)]
+    IC_prev_PO[:,i] = PO_data['IC_prev'].iloc[runs2plot[i],:]
+    IC_prev_avg_PO[15:-15,i] = PO_data['IC_prev_avg'].iloc[runs2plot[i],15:-15]
+    IC_ex_PO[:,i] = PO_data['IC_ex'].iloc[runs2plot[i],:]
 
 # Plot
 fig = plt.figure('QoIs',figsize=[14,5])
@@ -72,29 +69,17 @@ ax1 = fig.add_subplot(132, ylabel='Prevalent cases in IC \n Moving average', xla
 ax2 = fig.add_subplot(133, ylabel='IC patient-days in excess', xlabel='Time (days)')
 
 for i in range(n_runs2plot):
-	ax0.plot(time, IC_prev_PO[:,i], lw=2)
-	ax1.plot(time, IC_prev_avg_PO[:,i], lw=2)
-	ax2.plot(time, IC_ex_PO[:,i], lw=2)
-	add circles to indicate QoIs
-	# first (and last) 14 components of the array are nan
-	circle_qoi1 = plt.Circle((14+np.argmax(IC_prev_avg_PO[~np.isnan(IC_prev_avg_PO[:,i]),i]), \
-		np.max(IC_prev_avg_PO[~np.isnan(IC_prev_avg_PO[:,i]),i])), \
-		radius=10, color='black', lw=2, fill=False)
-	ax1.add_artist(circle_qoi1)
-	circle_qoi2 = Ellipse((L, np.max(IC_ex_PO[:,i])), \
-		width=20, height=1000, color='black', lw=2, fill=False)
-	ax2.add_artist(circle_qoi2)
-
-	# # indicate the QoIs with an arrow
-	# ax1.arrow(x=14+np.argmax(IC_prev_avg_PO[~np.isnan(IC_prev_avg_PO[:,i]),i])+30, \
-	# 	y=np.max(IC_prev_avg_PO[~np.isnan(IC_prev_avg_PO[:,i]),i])+30, dx=-20, dy=-20, lw=2, \
-	# 	head_width=6, shape='full')
-	# if i<=1:
-	# 	ax2.arrow(x=L-30, y=np.max(IC_ex_PO[:,i])+900, dx=20, dy=-600, lw=2, \
-	# 		head_width=6, shape='full')		
-	# else:
-	# 	ax2.arrow(x=L-30, y=np.max(IC_ex_PO[:,i])-900, dx=20, dy=600, lw=2, \
-	# 		head_width=6, shape='full')
+    ax0.plot(time, IC_prev_PO[:,i], lw=2)
+    ax1.plot(time[15:-15], IC_prev_avg_PO[15:-15,i], lw=2)
+    ax2.plot(time, IC_ex_PO[:,i], lw=2)
+    # add circles to indicate QoIs
+    circle_qoi1 = plt.Circle((np.argmax(IC_prev_avg_PO[~np.isnan(IC_prev_avg_PO[:,i]),i]), \
+        np.max(IC_prev_avg_PO[~np.isnan(IC_prev_avg_PO[:,i]),i])), \
+        radius=10, color='black', lw=2, fill=False)
+    ax1.add_artist(circle_qoi1)
+    circle_qoi2 = Ellipse((L, np.max(IC_ex_PO[:,i])), \
+        width=20, height=1000, color='black', lw=2, fill=False)
+    ax2.add_artist(circle_qoi2)
 
 ax0.hlines(y=IC_capacity, xmin=0, xmax=L, lw=2, ls=':', color='black')
 ax1.hlines(y=IC_capacity, xmin=0, xmax=L, lw=2, ls=':', color='black')
@@ -107,6 +92,6 @@ ax1.set_yticks([0, 200, 400])
 ax2.set_yticks([0, 10000, 20000])
 
 plt.tight_layout()
-fig.savefig('figures/QoIs.pdf')
+fig.savefig('figures/QoIs_test.pdf')
 
 plt.show()

98ed71a779963fa47824b97660725dc29e913c58
Federica Gugole <federica.gugole@cwi.nl>
2021-03-09 12:23:42 +0100

    change name figure file

--- EasyVVUQ/sobols_total.py
+++ EasyVVUQ/sobols_total.py
@@ -240,6 +240,6 @@ ax_PO.set_yticks(np.arange(0, len(PO_labels), 1))
 ax_PO.set_yticklabels(PO_labels)
 
 plt.tight_layout()
-f.savefig('figures/Sobols.pdf')
+f.savefig('figures/Sobols_total.pdf')
 
 plt.show()

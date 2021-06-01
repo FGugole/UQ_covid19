47e59199d850e382784f1b6ab8774a96c4310059
Federica Gugole <federica.gugole@cwi.nl>
2021-06-01 14:57:46 +0200

    update spaghetti plot

--- EasyVVUQ/spaghetti_plot.py
+++ EasyVVUQ/spaghetti_plot.py
@@ -21,17 +21,37 @@ L = 551
 time = np.arange(start=0, stop=L, step=1)
 IC_capacity = 109
 
-n_runs = 16
+n_runs = 100
+
+# store in a matrix the moving average of IC_prev (each column is a different simulation)
+IC_prev_avg = np.zeros((L,n_runs), dtype='float')
+for i in range(n_runs):
+    #IC_prev_avg[:,i] = np.copy(data.IC_prev.iloc[i*L:(i+1)*L].to_numpy())
+    IC_prev_avg[:,i] = data.IC_prev.iloc[i*L:(i+1)*L].rolling(window=30, center=True).mean()
+
+# store few individual realizations to plot
+runs2plot = [18, 7, 45, 27, 69]
+IC_prev_avg_2plot = np.zeros((L,len(runs2plot)), dtype='float')
+for i in range(len(runs2plot)):
+    IC_prev_avg_2plot[:,i] = np.copy(IC_prev_avg[:,runs2plot[i]])
+    
+# sort values to detect 5th-95th percentile interval
+IC_prev_avg.sort(axis=1)
+
 fig = plt.figure()
-ax = fig.add_subplot(111, ylabel='Prevalent cases in IC', xlabel='Time (days)')
+ax = fig.add_subplot(111, ylabel='Moving average of the prevalent cases in IC per million capita', xlabel='Time (days)')
 ax.set_xticks([0, 100, 200, 300, 400, 500])
 ax.set_yticks([0, 40, 80, 120, 160, 200])
-for i in range(n_runs):
-    ax.plot(time,data.IC_prev.iloc[i*L:(i+1)*L].to_numpy(), lw=2, c='black')
 
-ax.hlines(y=IC_capacity, xmin=0, xmax=L, lw=2, ls=':', color='red')
+ax.fill_between(time, IC_prev_avg[:,5], IC_prev_avg[:,94], color = 'lightgray')
+
+for i in range(len(runs2plot)):
+    ax.plot(time, IC_prev_avg_2plot[:,i], linewidth=2)
+
+ax.hlines(y=IC_capacity, xmin=0, xmax=L, lw=2, ls=':', color='black')
 
 plt.tight_layout()
+
 fig.savefig('figures/S5Fig_spaghetti_plot.eps')
 
 plt.show()
